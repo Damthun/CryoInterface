8554ef724152849f1810784da8af0749829bb46c
Dametre Thunberg <40583179+Damthun@users.noreply.github.com>
2024-02-15 19:57:41 -0600

    Revamped Vna_s2p;
    Removed loop based approach to gathering file;
    block format transfer bring reliability from 10% to practically 100% with ability to transfer every second.
    
    Removed uneccesary line check and replaced with expected size output from revamped "received_bytes" command.
    
    Revamped Vna_csv;
    Removed loop based approach much like vna_s2p;
    Removed check for begins and ends as it made gathering s1p files impossible.
    
    Added functionality for the E5061B ENA: utilizing changes to app thread, handler, & metadata along with js/html.

--- vna_funcs.py
+++ vna_funcs.py
@@ -3,8 +3,8 @@ import csv
 import logging
 import socket
 import time
-
-from vna import send_cmd
+import sys
+from vna import send_cmd, receive_bytes, build_cmd
 
 
 def ping_vna(s: socket.socket) -> bool:
@@ -13,9 +13,9 @@ def ping_vna(s: socket.socket) -> bool:
     """
     try:
         # Ask the VNA to identify itself.
-        send_cmd(s, cmd='*IDN?')
+        send_cmd(s, cmd='*IDN?\n')
         # Read the response back.
-        recv = s.recv(2048)
+        recv = s.recv(255)
         return True
     except:
         logging.exception('Error.')
@@ -25,97 +25,81 @@ def ping_vna(s: socket.socket) -> bool:
             logging.exception('Error closing connection.')
         return False
 
-def vna_s2p(s: socket.socket, resolution: int, fpath: str) -> bool:
-    # copy s2p file to computer
-    # Example resolution input = 201 (number of data points)
 
-    # 12 additional rows for data
-    expected_size = resolution + 12
+def vna_s2p(s: socket.socket, fpath: str, vna_type) -> bool:
+    # copy s2p file to computer
 
     # Save trace into .s2p file on the VNA
-    send_cmd(s, cmd='MMEM:STOR:SNP "CryoIntS.s2p"')
+    s.send(build_cmd(cmd='MMEM:STOR:SNP "CryoIntS.s2p"\n'))
 
     # The transfer of data is an inconsistent task. This while loop was put in place to repeat it until the data is successfully written into a file
-    for _ in range(10):
-        send_cmd(s, cmd='MMEM:DATA? "CryoIntS.s2p"')
+    for _ in range(1):
+        if vna_type == 'E5061B':
+            send_cmd(s, cmd='MMEM:TRAN? "CryoIntS.s2p"\n')
+            print('Sent command: MMEM:TRAN? "CryoIntS.s2p"\n')
 
-        recv = s.recv(1000000) #recv data, arg sets max number of bytes
+        elif vna_type == 'N9914A' or vna_type == 'N9913A':
+            send_cmd(s, cmd='MMEM:DATA? "CryoIntS.s2p"\n')
+            print('Sent command: MMEM:DATA? "CryoIntS.s2p"\n')
+
+        numbytes, recv = receive_bytes(s)
         contents = recv.decode('utf-8')
-
-        try:
-            idx = contents.index('!')
-            contents = contents[idx:]
-        except ValueError:
-            time.sleep(1)
-            print('Trying again...')
-            continue
+        print(len(recv))
 
         lines = contents.rstrip().split('\r\n')
-        
-        # print(f'expected={expected_size}; actual={len(lines)}')
 
         with open(fpath, 'w', encoding='utf-8') as wf:
             for line in lines:
                 wf.write(line+'\n')
 
-        if len(lines) == expected_size:
+        if abs(int(numbytes) - len(recv)) < 30:
             print('Success!')
             return True
         else:
-            time.sleep(1)
+            time.sleep(10)
             print('Trying again...')
             continue
 
     # return True #transfer is good, return True
-    print('Failed after 10 tries.')
+    print('Transfer Failed...')
     return False
 
 
-def vna_csv(s: socket.socket, fpath: str) -> bool:
-    send_cmd(s, cmd='MMEM:STOR:FDAT "CryoIntC.csv"')
-    print('Sent command: MMEM:STOR:FDAT "CryoIntC.csv"')
+def vna_csv(s: socket.socket, fpath: str, vna_type) -> bool:
+    s.send(build_cmd(cmd='MMEM:STOR:FDAT "CryoIntC.csv"\n'))
 
-    for _ in range(10):
+    for _ in range(1):
 
         # The transfer of data is an inconsistent task. This while loop was put in place to repeat it until the data is successfully written into a file
         # while True:
-        send_cmd(s, cmd='MMEM:DATA? "CryoIntC.csv"')
-        print('Sent command: MMEM:DATA? "CryoIntC.csv"')
+        if vna_type == 'E5061B':
+            send_cmd(s, cmd='MMEM:TRAN? "CryoIntC.csv"\n')
+            print('Sent command: MMEM:TRAN? "CryoIntC.csv"\n')
 
-        recv = s.recv(20000000) #recv data, arg sets max number of bytes
+        elif vna_type == 'N9914A' or vna_type == 'N9913A':
+            send_cmd(s, cmd='MMEM:DATA? "CryoIntC.csv"\n')
+            print('Sent command: MMEM:DATA? "CryoIntC.csv"\n')
+
+
+        numbytes, recv = receive_bytes(s)
+
+        #recv = s.recv(20000000)  #recv data, arg sets max number of bytes
         contents = recv.decode('utf-8')
-
-        try:
-            idx = contents.index('!')
-            contents = contents[idx:]
-        except ValueError:
-            time.sleep(1)
-            continue
-
-        bc = contents.count('BEGIN')
-        ec = contents.count('END')
-        if bc != 4 or ec != 4:
-            print(f'BEGIN COUNT: {bc}; END COUNT: {ec}')
-            print('File did not fully transfer.')
-            time.sleep(1)
-            continue
 
         # The transfer would first write some artifact type number usually something like #533281.
         # The following code removes the characters before the first '!' which marks the start of the information we want
-        try:
-            begin_idx = contents.index('BEGIN')
-            end_idx = contents.rindex('END')
-        except ValueError:
-            time.sleep(1)
-            continue
-        contents = contents[0:end_idx+3]
 
         with open(fpath, 'w', encoding='utf-8') as csv_wf:
             for line in contents.split('\n'):
                 csv_wf.write(line)
 
-        print('Success!')
-        return True
-    
+        if abs(int(numbytes) - len(recv)) < 30:
+            print('Success!')
+            return True
+        else:
+            time.sleep(10)
+            print('Trying again...')
+            continue
+
     print('Failed after 10 retries.')
     return False
