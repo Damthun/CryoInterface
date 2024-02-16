5cffcc0241b755637858f7d49d5a53a9127eb7c5
Dametre Thunberg <40583179+Damthun@users.noreply.github.com>
2024-02-15 19:55:38 -0600

    removed addition of newline character to build_cmd Need to deprecate command entirely.
    Redefined send_cmd to use s.sendall, removing need to check len(encoded)
    
    Created function receive_bytes;
    This function is tailored to the IEEE BLock format data transmission #ABC.
    This format is self describing and allows for easier data transfer.

--- vna.py
+++ vna.py
@@ -6,12 +6,36 @@ VNA_PORT = 5025
 
 
 def build_cmd(cmd: str) -> bytes:
-    cmd = cmd + '\n'
+    cmd = cmd
     return cmd.encode('utf-8')
 
 
 def send_cmd(s: socket.socket, cmd: str):
-    encoded = build_cmd(cmd)
-    bytes_sent = s.send(encoded)
-    if bytes_sent != len(encoded):
-        raise RuntimeError('Not all data was sent.')
+    cmd = cmd.encode('utf-8')
+    bytes_sent = s.sendall(cmd)
+    return
+
+
+def receive_bytes(s: socket.socket):
+    junkheader = s.recv(1)
+    print('1 :' + junkheader.decode('utf-8'))
+    if junkheader.decode('utf-8') != '#':
+        junk = s.recv(1)
+        print(junk.decode('utf-8'))
+    print(type(junkheader))
+
+    numOfDigitsByte = s.recv(1)
+    print(numOfDigitsByte)
+
+    numOfDigits = numOfDigitsByte.decode('utf-8')
+    print('3: ' + numOfDigits)
+
+    numBytes = s.recv(int(numOfDigits))
+    print(numBytes.decode('utf-8'))
+
+    NumBytesStr = numBytes.decode('utf-8')
+
+    data = s.recv(int(NumBytesStr))
+    while len(data) < int(NumBytesStr):
+        data += s.recv(1024)
+    return NumBytesStr, data
